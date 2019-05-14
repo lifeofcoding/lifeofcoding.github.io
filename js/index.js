4ea6273e0d2345f7a176bcfd8154c92e278d650f
LifeOfCoding <lifeofcodding@gmail.com>
2019-05-13 21:55:01 -0400

    vhosts typo 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
 
-  setTimeout(function() {
-    $('#particles-background').addClass('change');
-  }, 3000);
-  // Add smooth scrolling to all links in navbar + footer link
+// Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#home']").on('click', function(event) {
 
     // Prevent default anchor click behavior
